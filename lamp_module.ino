10bf04b3089726091946124fc3797c0633b52d32
Pratik-sys <pithore.pratik@gmial.com>
2019-09-20 22:57:18 +0530

    update the code * Edit the code for realy, relay works in different way. * Formated the code looks perfect now.
    Signed-off-by: Pratik-sys <pithore.pratik@gmial.com>

--- lamp_module.ino
+++ lamp_module.ino
@@ -1,19 +1,3 @@
-/***************************************************
-  Adafruit ESP8266 Lamp Controller Module
-  
-  Must use ESP8266 Arduino from:
-    https://github.com/esp8266/Arduino
-  Works great with Adafruit's Huzzah ESP board:
-  ----> https://www.adafruit.com/product/2471
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-  Written by Tony DiCola for Adafruit Industries.
-  Adafruit IO example additions by Todd Treece.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-
-// Libraries
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
@@ -22,14 +6,14 @@
 const int lamp_pin = 5;
 
 // WiFi parameters
-#define WLAN_SSID       "WLAN_SSID"
-#define WLAN_PASS       "WLAN_PASS"
+#define WLAN_SSID "WLAN_SSID"
+#define WLAN_PASS "WLAN_PASS"
 
-// Adafruit IO
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "AIO_USERNAME"
-#define AIO_KEY         "AIO_KEY"
+// Adafruit IO paramaters
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 1883
+#define AIO_USERNAME "AIO_USERNAME"
+#define AIO_KEY "AIO_KEY"
 
 // Functions
 void connect();
@@ -39,16 +23,17 @@ WiFiClient client;
 
 // Store the MQTT server, client ID, username, and password in flash memory.
 // This is required for using the Adafruit MQTT library.
-const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+const char MQTT_SERVER[] PROGMEM = AIO_SERVER;
 // Set a unique MQTT client ID using the AIO key + the date and time the sketch
 // was compiled (so this should be unique across multiple devices for a user,
 // alternatively you can manually set this to a GUID or other random value).
-const char MQTT_CLIENTID[] PROGMEM  = AIO_KEY __DATE__ __TIME__;
-const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
-const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+const char MQTT_CLIENTID[] PROGMEM = AIO_KEY __DATE__ __TIME__;
+const char MQTT_USERNAME[] PROGMEM = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM = AIO_KEY;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
 /****************************** Feeds ***************************************/
 
 // Setup a feed called 'lamp' for subscribing to changes.
@@ -58,23 +43,27 @@ Adafruit_MQTT_Subscribe lamp = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/fee
 
 /*************************** Sketch Code ************************************/
 
-void setup() {
+void setup()
+{
 
   // Set lamp pin to output
   pinMode(lamp_pin, OUTPUT);
+  digitalWrite(lamp_pin, HIGH);
 
   Serial.begin(115200);
 
   Serial.println(F("Adafruit IO Example"));
 
   // Connect to WiFi access point.
-  Serial.println(); Serial.println();
+  Serial.println();
+  Serial.println();
   delay(10);
   Serial.print(F("Connecting to "));
   Serial.println(WLAN_SSID);
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(F("."));
   }
@@ -89,24 +78,27 @@ void setup() {
 
   // connect to adafruit io
   connect();
-
 }
 
-void loop() {
+void loop()
+{
 
   Adafruit_MQTT_Subscribe *subscription;
 
-  // ping adafruit io a few times to make sure we remain connected
+  // ping adafruit io to make sure we remain connected
 
-    if(! mqtt.connected()){
-      connect();
+  if (!mqtt.connected())
+  {
+    connect();
   }
 
   // this is our 'wait for incoming subscription packets' busy subloop
-  while (subscription = mqtt.readSubscription(1000)) {
+  while (subscription = mqtt.readSubscription(1000))
+  {
 
     // we only care about the lamp events
-    if (subscription == &lamp) {
+    if (subscription == &lamp)
+    {
 
       // convert mqtt ascii payload to int
       char *value = (char *)lamp.lastread;
@@ -116,42 +108,60 @@ void loop() {
       // Apply message to lamp
       String message = String(value);
       message.trim();
-      if (message == "ON") {digitalWrite(lamp_pin, HIGH);}
-      if (message == "OFF") {digitalWrite(lamp_pin, LOW);}
-
+      if (message == "ON")
+      {
+        digitalWrite(lamp_pin, LOW);
+      }
+      if (message == "OFF")
+      {
+        digitalWrite(lamp_pin, HIGH);
+      }
     }
-
   }
-
 }
 
 // connect to adafruit io via MQTT
-void connect() {
+void connect()
+{
 
   Serial.print(F("Connecting to Adafruit IO... "));
 
   int8_t ret;
 
-  while ((ret = mqtt.connect()) != 0) {
+  while ((ret = mqtt.connect()) != 0)
+  {
 
-    switch (ret) {
-      case 1: Serial.println(F("Wrong protocol")); break;
-      case 2: Serial.println(F("ID rejected")); break;
-      case 3: Serial.println(F("Server unavail")); break;
-      case 4: Serial.println(F("Bad user/pass")); break;
-      case 5: Serial.println(F("Not authed")); break;
-      case 6: Serial.println(F("Failed to subscribe")); break;
-      default: Serial.println(F("Connection failed")); break;
+    switch (ret)
+    {
+    case 1:
+      Serial.println(F("Wrong protocol"));
+      break;
+    case 2:
+      Serial.println(F("ID rejected"));
+      break;
+    case 3:
+      Serial.println(F("Server unavail"));
+      break;
+    case 4:
+      Serial.println(F("Bad user/pass"));
+      break;
+    case 5:
+      Serial.println(F("Not authed"));
+      break;
+    case 6:
+      Serial.println(F("Failed to subscribe"));
+      break;
+    default:
+      Serial.println(F("Connection failed"));
+      break;
     }
 
-    if(ret >= 0)
+    if (ret >= 0)
       mqtt.disconnect();
 
     Serial.println(F("Retrying connection..."));
     delay(5000);
-
   }
 
   Serial.println(F("Adafruit IO Connected!"));
-
 }
